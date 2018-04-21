920d6fa3c45d635c4eb780bce042c6e40cd59d58
Haydart <makowiecki.radoslaw@gmail.com>
2018-04-21 15:30:57 +0200

    Print all filenames under a certain directory

--- data_cleaning/data_cleaner.py
+++ data_cleaning/data_cleaner.py
@@ -6,7 +6,11 @@ sanitized_html = sanitizer.sanitize('<html>sample text</html>')
 
 articles_path = '../org_dataset/articles'
 not_labels = ["__init__.py", "Artykuly.csv"]
-articles_labels = [dir_name for dir_name in os.listdir(articles_path) if dir_name not in not_labels]
+article_directories = [dir_name for dir_name in os.listdir(articles_path) if dir_name not in not_labels]
 
-print(articles_labels)
+print(article_directories)
 
+for directory in article_directories:
+    print(directory)
+    files_under_directory = os.listdir(articles_path + "/" + directory)
+    print(files_under_directory)
