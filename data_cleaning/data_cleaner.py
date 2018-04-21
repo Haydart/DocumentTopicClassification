ca2f682cd69b4b51ede04e6f0c64603257696abf
Haydart <makowiecki.radoslaw@gmail.com>
2018-04-21 15:28:58 +0200

    List all label directories

--- data_cleaning/data_cleaner.py
+++ data_cleaning/data_cleaner.py
@@ -1,3 +1,12 @@
 from html_sanitizer import Sanitizer
+import os
+
 sanitizer = Sanitizer()  # default configuration
 sanitized_html = sanitizer.sanitize('<html>sample text</html>')
+
+articles_path = '../org_dataset/articles'
+not_labels = ["__init__.py", "Artykuly.csv"]
+articles_labels = [dir_name for dir_name in os.listdir(articles_path) if dir_name not in not_labels]
+
+print(articles_labels)
+
