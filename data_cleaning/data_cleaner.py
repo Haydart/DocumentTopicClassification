88bceb3f3459a3b0f88cb1ffa1480cf2142d5163
Haydart <makowiecki.radoslaw@gmail.com>
2018-04-21 18:52:37 +0200

    Save cleaned text to file

--- data_cleaning/data_cleaner.py
+++ data_cleaning/data_cleaner.py
@@ -10,22 +10,32 @@ preprocessed_articles_path = '../preprocessed_dataset/articles'
 not_labels = ["__init__.py", "Artykuly.csv"]
 article_directories = [dir_name for dir_name in os.listdir(org_articles_path) if dir_name not in not_labels]
 
-print(article_directories)
+x = []
+labels = []
+i=0
+
+# print(article_directories)
 
 for directory in article_directories:
-    print(directory)
     files_under_directory = os.listdir(org_articles_path + "/" + directory)
-    print(files_under_directory)
 
     for filename in files_under_directory:
         with open(preprocessed_articles_path + "/" + directory + "/" + filename, 'w+') as outfile:
             with open(org_articles_path + "/" + directory + "/" + filename, 'r', encoding='utf-8') as infile:
-                input_text = infile.read()
-                sanitized_html = sanitizer.sanitize(input_text)
-                plain_text = html2text.html2text(sanitized_html)
-                cleaned_text = re.sub(r'/[/W+]*/[a-zA-Z0-9_%:!@#$^&*()-;\'\"/`~,.]*[ |\\|\n\r]', '', plain_text, flags=re.MULTILINE)
-                cleaned_text = re.sub(r'^https?:\/\/.*[\r\n]*', '', cleaned_text, flags=re.MULTILINE)
-                cleaned_text = re.sub(r'\W+', ' ', cleaned_text)
-                cleaned_text = re.sub(' +', ' ', cleaned_text)
-                cleaned_text = re.sub('_', '', cleaned_text)
-                outfile.writelines(cleaned_text)
+                with open('cleaned_texts', 'a+') as file:
+                    input_text = infile.read()
+                    sanitized_html = sanitizer.sanitize(input_text)
+                    plain_text = html2text.html2text(sanitized_html)
+                    text_without_whitespaces = re.sub(r"\s+", "", plain_text)
+                    cleaned_text = re.sub(r'/[A-Za-z0-9]*/[a-zA-Z0-9_%:!@#$^&*()?;\'\"`~,.=+-]*[ \\\n\r\t]', '', plain_text, flags=re.MULTILINE)
+                    cleaned_text = re.sub(r'https?:\/\/.*[\r\n]*', '', cleaned_text, flags=re.MULTILINE)
+                    cleaned_text = re.sub(r'\W+', ' ', cleaned_text)
+                    cleaned_text = re.sub(' +', ' ', cleaned_text)
+                    cleaned_text = re.sub('_', '', cleaned_text)
+                    outfile.writelines(cleaned_text)
+
+                    x.append(cleaned_text)
+                    labels.append(directory)
+                    file.write(cleaned_text + ',')
+                    print(i)
+                    i+=1
